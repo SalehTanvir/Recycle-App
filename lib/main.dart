b35cf613a1a1eabf63f09bc636a1d56f5d83c9b6
SalehTanvir <salehtanvir852@gmail.com>
2025-09-02 23:04:09 +0600

    Item Upload Page

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:recycle_app/pages/login.dart';
+import 'package:recycle_app/pages/upload_item.dart';
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
@@ -23,7 +24,7 @@ class MyApp extends StatelessWidget {
   
       colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: Login()
+      home: UploadItem()
     );
   }
 }
---
+++ lib/pages/upload_item.dart
@@ -0,0 +1,118 @@
+import 'package:flutter/material.dart';
+import 'package:recycle_app/services/widget_support.dart';
+
+class UploadItem extends StatefulWidget {
+  const UploadItem({super.key});
+
+  @override
+  State<UploadItem> createState() => _UploadItemState();
+}
+
+class _UploadItemState extends State<UploadItem> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Container(
+        margin: EdgeInsets.only(top: 40.0),
+        child: Column( children: [
+         Padding(
+           padding: const EdgeInsets.only(left: 20.0),
+           child: Row(
+             children: [
+              Material(
+                elevation: 3.0,
+                borderRadius: BorderRadius.circular(60),
+                child: Container(
+                  padding: EdgeInsets.all(6),
+                  decoration: BoxDecoration(color: Colors.black, borderRadius: BorderRadius.circular(60),
+                ),
+                child: Icon(Icons.arrow_back_ios_new_rounded, color: Colors.white,),),
+              ),
+              SizedBox(width: MediaQuery.of(context).size.width/5 ,),
+               Text("Upload Item", style: AppWidget.headlinetextstyle(20.0),),
+             ],
+           ),
+         ),
+         SizedBox(height: 20.0,),
+          Expanded(
+            child: Container(
+              width: MediaQuery.of(context).size.width,
+              decoration: BoxDecoration(borderRadius: BorderRadius.only(topLeft: Radius.circular(30), topRight: Radius.circular(30)),color: Color.fromARGB(255, 227, 227, 249)),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(height:40.0),
+                  Center(
+                    child: Container(
+                      height: 150,
+                      width: 150,
+                      decoration: BoxDecoration(color: Colors.white, border: Border.all(color: Colors.black, width: 2.0), borderRadius: BorderRadius.circular(20.0)),
+                      child: Icon(Icons.camera_alt_outlined),
+                    ),
+                  ),
+                  SizedBox(height: 40.0,),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 20.0,right: 20.0),
+                    child: Text("Enter Your Address you want the item to be picked.", style: AppWidget.normaltextstyle(20.0),),
+                  ),
+                  SizedBox(height: 10.0,),
+                  Container(
+                     margin: EdgeInsets.only(left: 20.0, right: 20.0),
+                    child: Material(
+                      elevation: 3.0,
+                      borderRadius: BorderRadius.circular(20.0),
+                      child: Container(
+                        decoration: BoxDecoration(color: Colors.white,borderRadius: BorderRadius.circular(20.0), ),
+                        child: TextField(
+                          decoration: InputDecoration(border: InputBorder.none,
+                          prefixIcon: Icon(Icons.location_on, color: Colors.green,),
+                          hintText: "Enter Address", hintStyle: AppWidget.normaltextstyle(20.0)
+                           ),
+                        ),
+                      ),
+                    ),
+                  ),
+                   SizedBox(height: 40.0,),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 20.0,right: 20.0),
+                    child: Text("Enter the Quantity of the item to be picked", style: AppWidget.normaltextstyle(20.0),),
+                  ),
+                  SizedBox(height: 10.0,),
+                  Container(
+                     margin: EdgeInsets.only(left: 20.0, right: 20.0),
+                    child: Material(
+                      elevation: 3.0,
+                      borderRadius: BorderRadius.circular(20.0),
+                      child: Container(
+                        decoration: BoxDecoration(color: Colors.white,borderRadius: BorderRadius.circular(20.0), ),
+                        child: TextField(
+                          decoration: InputDecoration(border: InputBorder.none,
+                          prefixIcon: Icon(Icons.inventory, color: Colors.green,),
+                          hintText: "Enter Quantity", hintStyle: AppWidget.normaltextstyle(20.0)
+                           ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 50.0,),
+                  Center(
+                    child: Material(
+                      elevation: 3.0,
+                       borderRadius: BorderRadius.circular(20.0),
+                      child: Container(
+                        height: 50.0,
+                        width: MediaQuery.of(context).size.width/2,
+                        decoration: BoxDecoration(color: Colors.green, borderRadius: BorderRadius.circular(20.0)),
+                        child: Center(child: Text("Upload",style: AppWidget.whitetextstyle(22.0),),),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+              ),
+          ),
+        ],),),
+    );
+  }
+}
