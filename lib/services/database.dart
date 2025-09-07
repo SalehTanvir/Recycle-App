c6002f26fe712a6b3dd8e6c0935ced134960cf68
SalehTanvir <salehtanvir852@gmail.com>
2025-09-07 23:45:43 +0600

    database up

--- lib/services/database.dart
+++ lib/services/database.dart
@@ -4,4 +4,19 @@ class DatabaseMethods{
   Future addUserInfo(Map<String,dynamic>userInfoMap, String id) async {
     return await FirebaseFirestore.instance.collection("users").doc(id).set(userInfoMap);
   }
+
+    Future addUserUploadItem(Map<String,dynamic>userInfoMap, String id, String itemid) async {
+    return await FirebaseFirestore.instance
+    .collection("users")
+    .doc(id)
+    .collection("Items").doc(itemid)
+    .set(userInfoMap);
+  }
+
+      Future addAdminItem(Map<String,dynamic>userInfoMap, String id, ) async {
+    return await FirebaseFirestore.instance
+    .collection("Request")
+    .doc(id)
+    .set(userInfoMap);
+  }
 }
